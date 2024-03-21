64b6c904a1faf5898bf28aeac9898b789f81835e
vinsmake <ps.enrique.e@gmail.com>
2024-03-21 11:31:54 -0600

    Project updated

--- src/UiTest.jsx
+++ src/UiTest.jsx
@@ -1,7 +1,6 @@
 import './assets/helpers/normalize.css'
 import { UiContainers } from "./ui/containers/UiContainers"
 import { TypeEffectComponent } from './ui/text/TypeEffect/TypeEffectComponent'
-import { TypeEffectArrayComponent } from './ui/text/TypeEffectArray/TypeEffectArrayComponent'
 
 
 
