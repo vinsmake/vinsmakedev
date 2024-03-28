71b002bc7d6c852036a4eaf048c2517831e307d8
vinsmake <ps.enrique.e@gmail.com>
2024-03-27 20:20:12 -0600

    Project updated

--- src/ui/hero/RychardR/RychardR.jsx
+++ src/ui/hero/RychardR/RychardR.jsx
@@ -5,30 +5,19 @@ import { TypeFlip } from '../../text/TypeFlip/TypeFlip'
 
 export const RychardR = () => {
 
-    const { hero1, hero2, hero3, logo } = RychardRData
+    const { hero1, hero2, hero3, logo} = RychardRData
 
     return (
         <>
             <section className="RychardR">
-
-                <img src={logo} className="RychardR-logo" />
-
                 <div className="RychardR-content">
-
+                    <img src={logo} className="RychardR-logo" />
                     <h1 className="RychardR-title">{hero1}</h1>
-
                     <div className="RychardR-subheader">
                         <p><TypeFlip propWords={['UX Designer', 'Full Stack Developer', 'React Native Developer',]} /></p>
                     </div>
-
                     <p className="RychardR-text">{hero3}</p>
-
-
-
                 </div>
-
-                {/*                 <img src="./assets/images/hero-shape.svg" width="211" height="189" alt="" class="shape" />
- */}
             </section>
         </>
     )
