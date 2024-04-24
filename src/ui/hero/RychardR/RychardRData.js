eb6654fb8811217d55a91e6ae97df13c31dd93f2
vinsmake <ps.enrique.e@gmail.com>
2024-04-23 20:24:10 -0600

    Project updated

--- src/ui/hero/RychardR/RychardRData.js
+++ src/ui/hero/RychardR/RychardRData.js
@@ -2,7 +2,7 @@ export const RychardRData =
 {
     'hero1': 'Enrique Plascencia',
     'hero2': 'Full Stack Developer',
-    'hero3': 'SINCE 2020, GDL, MX',
+    'hero3': 'DEVELOPING THE NEW WORLD',
     'logo': './images/rychardr/logo.png',
     'signature': './images/rychardr/signature.png'
 }
