02778d0b5bf27281276189603a9b3be624b8f6be
vinsmake <ps.enrique.e@gmail.com>
2024-05-02 18:14:14 -0600

    Project updated

--- src/ui/buttons/NightClubButton.jsx
+++ src/ui/buttons/NightClubButton.jsx
@@ -3,6 +3,7 @@ import './nightClubButton.css'
 export const NightClubButton = () => {
     return (
 
+
         <button class='glowing-btn'>
             <span class='glowing-txt'>S<span class='faulty-letter'>ave</span> Contact</span>
         </button>
